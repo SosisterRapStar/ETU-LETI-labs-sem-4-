d7867f11b77e34c64e5c18abd79b2cadeffdcc25
vanya <timoshenkovvanya@gmail.com>
2024-02-10 16:15:28 +0300

    first-commit

---
+++ lab_1/lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <pthread.h>
+
+
+//  int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+//                           void *(*start_routine) (void *), void *arg);
+
+//        Compile and link with -pthread.
+
+int main(){
+
+}
