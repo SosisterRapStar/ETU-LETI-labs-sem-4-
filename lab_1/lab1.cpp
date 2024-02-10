68ea69a18ffcc73794fb1a42433ab0180293ed94
vanya <timoshenkovvanya@gmail.com>
2024-02-10 19:01:33 +0300

    lab1_v_1

--- lab_1/lab1.cpp
+++ lab_1/lab1.cpp
@@ -1,12 +1,81 @@
 #include <iostream>
 #include <pthread.h>
+#include <unistd.h>
+#include <cstdio>
 
 
-//  int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-//                           void *(*start_routine) (void *), void *arg);
+static void * threadFunc_1 (void *flag){
+    printf("Поток 1 начал свою работу\n");
 
-//        Compile and link with -pthread.
+    sleep(1);
 
-int main(){
+    while (flag == 0) {
+        putchar('1');
+        fflush(stdout);
+        sleep(1);
+        
+    }
+
+    pthread_exit((void*)1);
 
 }
+
+static void * threadFunc_2 (void *flag){
+    printf("Поток 2 начал свою работу\n");
+
+    sleep(1);
+
+    while (flag == 0) {
+        putchar('2');
+        fflush(stdout);
+        sleep(1);
+        
+    }
+
+    pthread_exit((void*)2);
+
+}
+
+void mainThreadErrorHandler(int* ret_val){
+    if(*ret_val != 0) {
+                perror("Ошибка создания потока"); 
+        }
+}
+
+
+int main(){
+    printf("Программа начала свою работу\n");
+
+    int flag1 = 0;
+    int flag2 = 0;
+
+    int ret_val;
+
+    pthread_t threadId_1;
+    pthread_t threadId_2;
+
+    ret_val = pthread_create(&threadId_1, NULL, threadFunc_1, (void *) flag1);
+    mainThreadErrorHandler(&ret_val);
+    ret_val = pthread_create(&threadId_2, NULL, threadFunc_2, (void *) flag2);
+    mainThreadErrorHandler(&ret_val);
+
+    printf("Программа ждет нажатия клавиши\n");
+
+    getchar();
+
+    printf("Клавиша нажата\n");
+
+    flag1 = 1;
+    flag2 = 1;
+
+    int *exitcode1;
+    int *exitcode2;
+
+    ret_val = pthread_join(threadId_1, (void**)&exitcode1);
+    ret_val = pthread_join(threadId_2, (void**)&exitcode2);
+
+    printf("exitcode1 = %p\n, exitcode2 = %p\n", exitcode1, exitcode2);
+
+    printf("Программа завершила свою работу");
+
+}  
